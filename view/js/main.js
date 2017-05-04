16df1ff51f7e741744c40d2e28a7ea9a19a24804
Franklin Hung <hbopresent@gmail.com>
2017-05-04 11:49:29 +0800

    UPDATE

--- view/js/main.js
+++ view/js/main.js
@@ -5,45 +5,81 @@ var screenlock = (function() {
    var password = document.getElementById("password");
    var signinButton = document.getElementById("signinButton");
    var desktop = document.getElementById("desktop");
+   var passedTimeMinute = document.getElementById("passedTimeMinute");
+   var passedTimeSecond = document.getElementById("passedTimeSecond");
    var playingMusicFlag = null;
-   var width = 20;
+   var playingStateWidth = 20;
+   var playedSecond = null;
+   var playedMinute = null;
 
    function bindListeners() {
      unlock.addEventListener("click", unlockScreen);
-    //  password.addEventListener("click", passwordOfSignin);
      signinButton.addEventListener("click", startMac);
+     window.addEventListener("keydown", function(e) {
+       if(e.code == "Enter") {
+         startMac();
+       }
+     });
    }
 
    function unlockScreen() {
      musicPlayerBoard.classList.add("disappearMusicPlayer");
      setTimeout(function() {
+       password.focus();
        unlockBoard.classList.add("displayUnlockBoard");
      }, 300);
    }
 
-   function passwordOfSignin() {
-
-   }
-
    function startMac() {
+     password.blur();
+     password.style.backgroundColor = "rgba(225, 225, 225, 0.05)";
+     signinButton.style.backgroundColor = "rgba(225, 225, 225, 0.05)";
      desktop.classList.add("displayDesktop");
    }
 
    function playingMusic() {
      playingMusicFlag = setInterval(function() {
-       console.log("hh");
-       playingState.style.width = width + "%";
-       width += 1;
+       if(playingStateWidth >= 100) {
+         clearInterval(playingMusicFlag);
+         return;
+       }
+       playingState.style.width = playingStateWidth + "%";
+       playingStateWidth += 0.518;
+       updateMusicTime();
      }, 1000);
    }
 
-   function stopPlayingMusic() {
+   //  update music time
+   function updateMusicTime() {
+     // update played seconds
+     playedSecond = parseInt(passedTimeSecond.innerHTML) + 1;
 
+     if(playedSecond == 60) {
+       playedSecond = 0;
+       passedTimeSecond.innerHTML = "0" + playedSecond;
+
+       // update played minutes
+       playedMinute = (parseInt(passedTimeMinute.innerHTML) + 1);
+       if(playedMinute < 10) {
+        passedTimeMinute.innerHTML = "0" + playedMinute + ":";
+       }
+       else {
+        passedTimeMinute.innerHTML = playedMinute + ":";
+       }
+     }
+     else {
+       if(playedSecond < 10) {
+         passedTimeSecond.innerHTML = "0" + playedSecond;
+       }
+       else {
+         passedTimeSecond.innerHTML = playedSecond;
+       }
+     }
    }
 
    function initialize() {
      bindListeners();
-    //  playingMusic();
+     playingMusic();
    }
 
    return {
